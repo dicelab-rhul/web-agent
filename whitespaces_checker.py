1567d0fa39d9c0b552e42599024e8359dfa1900f
cloudstrife9999 <emanuele.uliana.90@gmail.com>
2023-02-10 15:02:29 +0000

    Minor.

--- whitespaces_checker.py
+++ whitespaces_checker.py
@@ -2,16 +2,28 @@
 
 import os
 
-for d, _, files in os.walk("."):
-    if "node_modules" in d or ".git" in d:
-        continue
 
+def main() -> None:
+    for d, _, files in os.walk("."):
+        if "node_modules" in d or ".git" in d:
+            continue
+        else:
+            __scan_files(d, files)
+
+def __scan_files(d: str, files: list[str]) -> None:
     for f in files:
         if f.endswith(".mp3") or f.endswith(".png") or f.endswith(".ico"):
             continue
         else:
-            lines = open(os.path.join(d, f)).readlines()
+            __scan_file(d, f)
 
-            for i in range(len(lines)):
-                if " \n" in lines[i]:
-                    print(os.path.join(d, f) + ": line " + str(i + 1) + ".")
+def __scan_file(d: str, f: str) -> None:
+    lines: list[str] = open(os.path.join(d, f)).readlines()
+
+    for i in range(len(lines)):
+        if " \n" in lines[i]:
+            print(os.path.join(d, f) + ": line " + str(i + 1) + ".")
+
+
+if __name__ == "__main__":
+    main()
