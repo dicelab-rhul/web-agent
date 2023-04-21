258b98dfe9f6ba1b5c6bb6979451411ac34a70c4
cloudstrife9999 <emanuele.uliana.90@gmail.com>
2023-04-21 12:33:23 +0100

    Completely replaced webpack with Parcel v2.

--- parcel.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if ! command -v parcel &> /dev/null; then
-    npm prune && npm install
-fi
-
-cp favicon.ico parcel_dist
-sleep 2 && cd parcel_dist && live-server &
-parcel watch
